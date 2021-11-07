c836a205f099d47edf28a1f7bca8f621c97ee1dd
kevin_smrt <kevinsanmartin@live.fr>
2021-11-07 18:54:19 +0100

    add mailcontroller and /mail route

--- app/router.js
+++ app/router.js
@@ -1,5 +1,8 @@
 // Récupère la classe Router du module express
 const { Router } = require("express");
+// Récupère le controller qui gère les mails
+const mailController = require('./controllers/mailController');
+
 
 // Initialisation du router
 const router = Router();
@@ -8,6 +11,9 @@ const router = Router();
 router.get("/", (req, res) => {
     res.status(200).sendFile(__dirname + "/views/index.html");
 });
+
+// Route pour envoyer un mail
+router.post("/mail", mailController.sendMail);
 
 // Si aucune route n'est trouvée => 404
 router.use((req, res) => {
