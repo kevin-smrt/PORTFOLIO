dbaabe811d6e2c5d91f95625b6ed14e6feecd916
kevin_smrt <kevinsanmartin@live.fr>
2021-11-07 19:12:15 +0100

    add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'server.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : '51.77.222.144',
+      ref  : 'origin/master',
+      repo : 'https://github.com/kevin-smrt/PORTFOLIO',
+      path : '/home/ubuntu/pm2-deploy',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
